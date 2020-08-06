ceab501b7e0f23ca37994ad3a25495fcc3d6ea17
Michael Whalon <Michael.Whalon.CS@gmail.com>
2020-08-06 15:14:54 -0400

    new API endpoints to create a class section/add a student to a section

--- Classroom/urls.py
+++ Classroom/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import current_user, UserList, all_reviews_by_user,all_reviews_by_professor, EventList, EventDetail, NewEvent,ProfileDetail,get_sections_for_current_user
+from .views import current_user, UserList, all_reviews_by_user,all_reviews_by_professor, EventList, EventDetail, NewEvent,ProfileDetail,get_sections_for_current_user, add_current_user_to_section,new_section
 
 
 urlpatterns = [
     path('current_user/', current_user),
     path('current_user/reviews/all', all_reviews_by_user),
     path('current_user/sections/all', get_sections_for_current_user),
+    path('current_user/sections/<int:SectionID>/AddAStudent', add_current_user_to_section),
     path('reviews/<ProfID>', all_reviews_by_professor),
+    path('sections/new', new_section),
     
     path('users/', UserList.as_view()),
 
